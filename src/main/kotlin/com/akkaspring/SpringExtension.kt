33b75af8db2f86b77fe611846fa22cfa267a5691
Luis Trigueiros <luistrigueiros@gmail.com>
2018-03-10 15:32:00 +0000

    Incremantal save

--- src/main/kotlin/com/akkaspring/SpringExtension.kt
+++ src/main/kotlin/com/akkaspring/SpringExtension.kt
@@ -6,22 +6,21 @@ import akka.actor.Extension
 import akka.actor.Props
 import org.springframework.context.ApplicationContext
 
-class SpringExt : Extension {
-
-    @Volatile
-    private var applicationContext: ApplicationContext? = null
-
-    fun initialize(applicationContext: ApplicationContext) {
-        this.applicationContext = applicationContext
-    }
-
-    fun props(actorBeanName: String): Props {
-        return Props.create(SpringActorProducer::class.java, applicationContext, actorBeanName)
-    }
-
-}
-
-
-object SpringExtension : AbstractExtensionId<SpringExt>() {
+object SpringExtension : AbstractExtensionId<SpringExtension.SpringExt>() {
     override fun createExtension(system: ExtendedActorSystem): SpringExt = SpringExt()
+
+    class SpringExt : Extension {
+
+        private lateinit var applicationContext: ApplicationContext
+
+        fun initialize(applicationContext: ApplicationContext) {
+            this.applicationContext = applicationContext
+        }
+
+        fun props(actorBeanName: String): Props {
+            return Props.create(SpringActorProducer::class.java, applicationContext, actorBeanName)
+        }
+
+    }
+
 }
